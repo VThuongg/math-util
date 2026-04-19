1d993113bb707219a7d5f3bfa16c09cf0ccf93e7
Thuong <thuong16052004@gmail.com>
2026-04-19 14:34:12 +0700

    update code to see green 4ever

--- test/com/thuong/mathutil/test/core/MathUtilityTest.java
+++ test/com/thuong/mathutil/test/core/MathUtilityTest.java
@@ -51,7 +51,7 @@ public class MathUtilityTest {
         Assert.assertEquals(2, MathUtil.getFactorial(2));   //tui muốn 2! = 2
         Assert.assertEquals(6, MathUtil.getFactorial(3));   //tui muốn 3! = 6
         Assert.assertEquals(24, MathUtil.getFactorial(4));   //tui muốn 4! = 24
-        Assert.assertEquals(120, MathUtil.getFactorial(6));   //tui muốn 5! = 120
+        Assert.assertEquals(120, MathUtil.getFactorial(5));   //tui muốn 5! = 120
         Assert.assertEquals(720, MathUtil.getFactorial(6));   //tui muốn 6! = 720
         
         //Hàm getF() ta thiết kế có 2 tình huống xử lý
