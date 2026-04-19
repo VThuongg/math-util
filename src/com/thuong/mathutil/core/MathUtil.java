f7abc91f04f6a5ba51302eff32e3f97d60f82951
Thuong <thuong16052004@gmail.com>
2026-04-19 21:32:31 +0700

    refactor the man code and green 4ever always

--- src/com/thuong/mathutil/core/MathUtil.java
+++ src/com/thuong/mathutil/core/MathUtil.java
@@ -22,16 +22,49 @@ public class MathUtil {
     //20 giai thừa là 18 con số 0, vừa kịp đủ cho kiểu long của Java
     //21 giai thừa tràn kiểu long
     //bài này quy ước tính n! trong khoảng từ 0..20
-    public static long getFactorial(int n) {
-        if (n < 0 || n > 20)
-            throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
+//        
+//        if (n == 1 || n == 0)
+//            return 1;   //kết thúc sớm nếu nhận những đầu vào đặc biệt
+//        
+//        long product = 1;   //tích nhân dồn, thuật toán heo đất, ốc bu nhồi thịt
+//        for(int i = 1; i <= n; i++)
+//            product *= i;
+//        return product;
+//    }
+//    
+    
+    //Học đệ quy trong vòng 30s - RECURSION
+    //Đệ quy là hiện tượng gọi lại chính mình với 1 quy mô khác
+    //Ví dụ: con búp bê Nga, giống nhau và lồng trong nhau
+    //búp bê to, nhỏ hơn, nhỏ hơn nữa, nhỏ hơn nữa...đến điểm dừng
+    
+    //tính dùm tui 6!
+    //n! = 1.2.3.4.5.6. ... .n
+    //6! = 6 * 5!
+    //5! = 5 * 4!
+    //4! = 4 * 3!
+    //3! = 3 * 2!
+    //2! = 2 * 1!   //điểm dừng
+    //1! = 1
+    
+    //quy ước 1! = 0! = 1;
+    
+    //chốt hạ: n! = n * (n - 1)!
+    
+    
+     public static long getFactorial(int n) {
+        
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("n must be between 0..20. Plz!");
+        }
         
         if (n == 1 || n == 0)
             return 1;   //kết thúc sớm nếu nhận những đầu vào đặc biệt
-        
-        long product = 1;   //tích nhân dồn, thuật toán heo đất, ốc bu nhồi thịt
-        for(int i = 1; i <= n; i++)
-            product *= i;
-        return product;
+        return n * getFactorial(n - 1); //công thức đệ quy ráp vào
     }
+    
+    
 }
